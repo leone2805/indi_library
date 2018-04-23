00f6e078ca6477fcf3ec8f5fec8eb4520ab686b4
leone <leone.dimario@gmail.com>
2018-04-23 10:57:19 +0200

    Test modifica da git

--- Utilities/liveOff.sh
+++ Utilities/liveOff.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+
 FORMAT=`indi_getprop "Canon DSLR EOS 1200D".CAPTURE_FORMAT.*|grep On|tail -1|cut -d . -f 3`
 indi_setprop "Canon DSLR EOS 1200D".CCD_VIDEO_STREAM.STREAM_OFF=On
 indi_setprop "Canon DSLR EOS 1200D".CONNECTION.DISCONNECT=On
+
 sleep 3
+
 indi_setprop "Canon DSLR EOS 1200D".CONNECTION.CONNECT=On
 indi_setprop "Canon DSLR EOS 1200D.CAPTURE_FORMAT.${FORMAT}"
