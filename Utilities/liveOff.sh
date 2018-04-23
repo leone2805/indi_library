cfdfa1c1cc2cbf68b6cb097e1d8ae02f63988b4f
leone <leone.dimario@gmail.com>
2018-04-23 14:01:55 +0200

    TestCommit2

--- Utilities/liveOff.sh
+++ Utilities/liveOff.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #Disable Liveview
+
 FORMAT=`indi_getprop "Canon DSLR EOS 1200D".CAPTURE_FORMAT.*|grep On|tail -1|cut -d . -f 3`
 indi_setprop "Canon DSLR EOS 1200D".CCD_VIDEO_STREAM.STREAM_OFF=On
 indi_setprop "Canon DSLR EOS 1200D".CONNECTION.DISCONNECT=On
